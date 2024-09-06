d70572cc60f0fd6ce466ce58ce9c9f2ce1944b12
User <dfofxadze2010@gmail.com>
2024-09-06 20:10:45 +0400

    commit

--- README.md
+++
@@ -1,1 +0,0 @@
-this is my GOA homeworks <3 take a look at them if you want to <3 :)
---
+++ day 076/homework/index.js
@@ -0,0 +1,6 @@
+const myParagraph = document.getElementById("myParagraph");
+
+myParagraph.style.backgroundColor = 'red'
+myParagraph.style.color = 'darkblue'
+
+console.log('Hello world');
