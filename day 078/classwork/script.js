1ffe8bc9b1002097d90a0e24c32a554e51950208
User <dfofxadze2010@gmail.com>
2024-09-08 14:39:51 +0400

    commit

---
+++ day 078/classwork/script.js
@@ -0,0 +1,21 @@
+function buttonContent(){
+    document.getElementById("myParagraph").textContent = 'now this says my paragraph';
+}
+
+
+
+
+function changeColorBlue(){
+    document.getElementById("myP").style.color = 'blue';
+    document.getElementById("myP").style.backgroundColor = 'lightblue';
+}
+
+function changeColorGreen(){
+    document.getElementById("myP").style.color = 'green';
+    document.getElementById("myP").style.backgroundColor = 'yellowGreen';
+}
+
+function changeColorYellow(){
+    document.getElementById("myP").style.color = 'yellow';
+    document.getElementById("myP").style.backgroundColor = 'orange';
+}
---
+++ day 078/homework/script.js
@@ -0,0 +1,30 @@
+let myGreen = document.getElementById('myDiv')
+
+let myRed = document.getElementById('myDiv')
+
+let myBlue = document.getElementById('myDiv')
+
+let myBlack = document.getElementById('myDiv')
+
+let myPink = document.getElementById('myDiv')
+
+
+function green(){
+    myGreen.style.backgroundColor = 'green'
+}
+
+function red(){
+    myRed.style.backgroundColor = 'red'
+}
+
+function blue(){
+    myBlue.style.backgroundColor = 'blue'
+}
+
+function black(){
+    myBlack.style.backgroundColor = 'black'
+}
+
+function pink(){
+    myPink.style.backgroundColor = 'pink'
+}
