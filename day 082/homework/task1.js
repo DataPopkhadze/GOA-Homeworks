281db5e7d6ffea87ecec8b51a8504c1b4d9eaae2
User <dfofxadze2010@gmail.com>
2024-09-12 14:59:54 +0400

    commit

---
+++ day 082/homework/task1.js
@@ -0,0 +1,12 @@
+let arr = [];
+
+function getElements(start, end){
+    let i = start.value;
+    let b = end.value;
+
+    arr.push(b);
+    arr.push(i);
+    
+}
+
+getElements(1, 20)
---
+++ day 084/script.js
@@ -0,0 +1,11 @@
+function Person(name, lastName, age, middleName) {
+    this.name = name;
+    this.lastName = lastName;
+    this.age = age;
+    this.middleName = middleName;
+}
+
+let datunia = new Person('data', 'popkhadze', 14, 'datunia');
+
+console.log(datunia);
+
