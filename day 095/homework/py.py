cdc415282f3a2cf401eac2bbc012652a7be36dcc
data fofxadze <dfofxadze2010@gmail.com>
2024-12-21 23:01:15 +0400

    commit

---
+++ day 095/homework/py.py
@@ -0,0 +1,23 @@
+def find_longest_increasing_subarray(arr):
+    if not arr:
+        return []
+
+    max_subarray = []
+    current_subarray = [arr[0]]
+
+    for i in range(1, len(arr)):
+        if arr[i] > arr[i - 1]:
+            current_subarray.append(arr[i])
+        else:
+            if len(current_subarray) > len(max_subarray):
+                max_subarray = current_subarray
+            current_subarray = [arr[i]]
+
+    if len(current_subarray) > len(max_subarray):
+        max_subarray = current_subarray
+
+    return max_subarray
+
+
+print(find_longest_increasing_subarray([1, 2, 3, 1, 2, 3, 4]))
+print(find_longest_increasing_subarray([1, 2, 1, 3, 16, 21, 11]))
---
+++ day 097/classwork/script.js
@@ -0,0 +1,10 @@
+const obj = {
+    name: "data",
+    surname: "popkhadze",
+    age: 14,
+}
+
+for(let elem in obj){
+    console.log(elem + ": " + obj[elem])
+}
+
