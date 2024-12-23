d67b1beb52ee4600b1fc9c75b8ae2da945e8e60a
data fofxadze <dfofxadze2010@gmail.com>
2024-12-23 22:10:42 +0400

    commit

--- day 097/homework/script.js
+++ day 097/homework/script.js
@@ -4,19 +4,60 @@ const emails = [
     { subject: "მოგზაურობა", content: "ძალიან კარგი შეთავაზება მაქვს", receivedDate: "2024-12-13" },
     { subject: "გაცნობიერება", content: "შესაძლებელია მცირე შეცდომები", receivedDate: "2024-12-10" }
   ];
-
+  
   const filterSelect = document.getElementById('filter');
   const applyFilterButton = document.getElementById('applyFilter');
   const emailList = document.getElementById('emailList');
-
-  applyFilterButton.addEventListener('click', function () {
-    const selectedSubject = filterSelect.value;
+  const addEmailButton = document.getElementById('addEmail');
+  const newSubjectInput = document.getElementById('newSubject');
+  const newContentInput = document.getElementById('newContent');
+  
+  function showEmails(filter) {
     emailList.innerHTML = '';
-    for (const email of emails) {
-      if (selectedSubject && email.subject.startsWith(selectedSubject)) {
+    for (let i = 0; i < emails.length; i++) {
+      if (!filter || emails[i].subject.startsWith(filter)) {
         const listItem = document.createElement('li');
-        listItem.textContent = `${email.subject}: ${email.content}`;
+        listItem.textContent = emails[i].subject + ": " + emails[i].content;
         emailList.appendChild(listItem);
       }
     }
+  }
+  
+  applyFilterButton.addEventListener('click', function () {
+    const selectedSubject = filterSelect.value;
+    showEmails(selectedSubject);
   });
+  
+  addEmailButton.addEventListener('click', function () {
+    const newSubject = newSubjectInput.value.trim();
+    const newContent = newContentInput.value.trim();
+  
+    if (newSubject && newContent) {
+      emails.push({ subject: newSubject, content: newContent});
+  
+      let exists = false;
+      for (let i = 0; i < filterSelect.options.length; i++) {
+        if (filterSelect.options[i].value === newSubject) {
+          exists = true;
+          break;
+        }
+      }
+  
+      if (!exists) {
+        const newOption = document.createElement('option');
+        newOption.value = newSubject;
+        newOption.textContent = newSubject;
+        filterSelect.appendChild(newOption);
+      }
+  
+      newSubjectInput.value = '';
+      newContentInput.value = '';
+      showEmails();
+    } 
+    else {
+      alert('ორივე შეიყვანე, subject-იც და content-იც.');
+    }
+  });
+  
+  showEmails();
+  
